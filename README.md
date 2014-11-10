b2eb816730a9439cc5f0286f3fe44b7f16bd72b8
Antoine <antoine.jaussoin@gmail.com>
2014-11-10 19:46:38 +0000

    Fixing a concurrency issue (on mac), improving doc

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ gulp-jimp
 
 > JavaScript Image Manipulation Program adapted to Gulp
 
+Disclaimer: this is a work in progress, only works with JPG for now 
+
 ## Install
 
 ```sh
@@ -17,7 +19,7 @@ var jimp = require('gulp-jimp');
 
 gulp.task('default', function () {
 	return gulp.src('src/images/*')
-		.pipe({ resize: { width: 200, height: 300 }, scale: 30, greyscale: true })
+		.pipe(jimp({ resize: { width: 200, height: 300 }, scale: 30, greyscale: true }))
 		.pipe(gulp.dest('dist'));
 });
 ```
@@ -30,3 +32,55 @@ You can define the transformations in the options, and add as many as you want (
 
 ## Crop
 
+```js
+{ crop: { x: 100, y: 100, width: 200, height: 200 } }
+```
+
+## Invert
+
+```js
+{ invert: true }
+```
+
+## Greyscale
+
+```js
+{ greyscale: true }
+```
+
+## Sepia
+
+```js
+{ sepia: true }
+```
+
+## Opacity
+
+```js
+{ opacity: 90 }
+```
+
+## Resize
+
+```js
+{ resize: { width: 100, height: 100 } }
+```
+
+## Scale
+
+```js
+{ scale: 90 }
+```
+
+
+## Blur
+
+```js
+{ blur: 90 }
+```
+
+## Gaussian
+
+```js
+{ gaussian: 90 }
+```
--- index.js
+++ index.js
@@ -9,57 +9,58 @@ module.exports = function (opts) {
         if (file.isNull()) return cb(null, file); // pass along
         if (file.isStream()) return cb(new Error('gulp-jimp: Streaming not supported'));
 
-        var tempFile = file.path + '.tmp.jpg';
-
         var image = new Jimp(file.path, function () {
 
             if (opts.resize)
                 this.resize(opts.resize.width, opts.resize.height);
-                
+
             if (opts.quality)
                 this.quality(opts.quality);
-            
+
             if (opts.greyscale)
                 this.greyscale();
-            
+
             if (opts.invert)
                 this.invert();
-            
+
             if (opts.sepia)
                 this.sepia();
-            
+
             if (opts.opacity)
                 this.opacity(opts.opacity);
-            
+
             if (opts.scale)
                 this.scale(opts.scale);
-            
+
             if (opts.blur)
                 this.blur(opts.blur);
-            
+
             if (opts.gaussian)
-                this.gaussian(opts.gaussian);   
-         
+                this.gaussian(opts.gaussian);
+
             if (opts.crop)
                 this.crop(opts.crop.x, opts.crop.y, opts.crop.width, opts.crop.height);
-            
 
 
+            var tempFile = file.path + '.tmp.jpg';
             this.write(tempFile, function () {
 
-                fs.readFile(tempFile, function (err, data) {
-                    if (err)
-                        return console.log(err);
+                setTimeout(function () {
+                    fs.readFileSync(tempFile, null, function (err, data) {
+                        if (err)
+                            return cb(new Error(err));
 
+                        file.contents = new Buffer(data);
 
-                    file.contents = new Buffer(data);
-                    fs.unlink(tempFile, function (err2) {
-                        if (err2)
-                            return console.log(err2);
+                        fs.unlink(tempFile, function (err2) {
+                            if (err2)
+                                return cb(new Error(err2));
 
-                        cb(null, file);
+                            cb(null, file);
+                        });
+
                     });
-                });
+                }, 0); // Concurrency issue
 
             });
         });
