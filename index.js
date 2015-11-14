802a522ef8af06cfc00b24b808f7012e935fde47
Hayden Bleasel <haydenbleasel@gmail.com>
2015-11-14 00:48:35 -0800

    Added new JIMP features

--- index.js
+++ index.js
@@ -8,10 +8,12 @@
         gutil = require('gulp-util'),
         Jimp = require('jimp');
 
-    module.exports = function (parameters) {
+    module.exports = function (options) {
 
         function print(message) {
-            console.log(message + '...');
+            if (options.logging) {
+                console.log(message + '...');
+            }
         }
 
         return through2.obj(function (file, encoding, callback) {
@@ -28,59 +30,129 @@
 
             Jimp.read(file.contents).then(function (image) {
 
-                if (parameters.resize) {
-                    print('Applying Resize of ' + parameters.resize.width + 'x' + parameters.resize.height);
-                    image.resize(parameters.resize.width, parameters.resize.height);
+                if (options.crop) {
+                    //print('Applying Crop of ' + options.crop.width + 'x' + options.crop.height + ' at ' + options.crop.x + ',' + options.crop.y);
+                    //pxData.copy(rawData, rawPos, pxPos, pxPos + byteWidth);: RangeError: out of range index
+                    //image.crop(options.crop.x, options.crop.y, options.crop.width, options.crop.height);
                 }
 
-                if (parameters.quality) {
-                    print('Applying Quality of ' + parameters.quality);
-                    image.quality(parameters.quality);
-                }
-
-                if (parameters.greyscale) {
-                    print('Applying Greyscale');
-                    image.greyscale();
-                }
-
-                if (parameters.invert) {
-                    print('Applying Invert');
+                if (options.invert) {
+                    print('Inverting image');
                     image.invert();
                 }
 
-                if (parameters.sepia) {
-                    print('Applying Sepia');
+                if (options.flip || options.mirror) {
+                    print('Flipping (mirroring) image ' + (options.flip.horizontal ? '' : 'not ') + 'horizontally and ' + (options.flip.vertical ? '' : 'not ') + 'vertically');
+                    image.flip(options.flip.horizontal, options.flip.vertical);
+                }
+
+                if (options.gaussian) {
+                    print('Applying gaussian blur of ' + options.gaussian);
+                    image.gaussian(options.gaussian);
+                }
+
+                if (options.blur) {
+                    print('Applying fast blur of ' + options.blur);
+                    image.blur(options.blur);
+                }
+
+                if (options.greyscale) {
+                    print('Applying greyscale');
+                    image.greyscale();
+                }
+
+                if (options.sepia) {
+                    print('Applying sepia tone');
                     image.sepia();
                 }
 
-                if (parameters.opacity !== undefined) {
-                    print('Applying Opacity of ' + parameters.opacity);
-                    image.opacity(parameters.opacity);
+                if (options.opacity !== undefined) {
+                    print('Changing opacity to ' + options.opacity);
+                    image.opacity(options.opacity);
                 }
 
-                if (parameters.scale) {
-                    print('Applying Scale of ' + parameters.scale);
-                    image.scale(parameters.scale);
+                if (options.resize) {
+                    print('Resizing image to ' + options.resize.width + 'x' + options.resize.height);
+                    image.resize(options.resize.width, options.resize.height);
                 }
 
-                if (parameters.blur) {
-                    print('Applying Blur of ' + parameters.blur);
-                    image.blur(parameters.blur);
+                if (options.scale) {
+                    print('Scaling image by ' + options.scale);
+                    image.scale(options.scale);
                 }
 
-                if (parameters.gaussian) {
-                    print('Applying Gaussian of ' + parameters.gaussian);
-                    image.gaussian(parameters.gaussian);
+                if (options.rotate) {
+                    print('Rotating image ' + options.rotate + ' degrees clockwise');
+                    image.rotate(options.rotate);
                 }
 
-                if (parameters.crop) {
-                    //print('Applying Crop of ' + parameters.crop.width + 'x' + parameters.crop.height + ' at ' + parameters.crop.x + ':' + parameters.crop.y);
-                    //pxData.copy(rawData, rawPos, pxPos, pxPos + byteWidth);: RangeError: out of range index
-                    //image.crop(parameters.crop.x, parameters.crop.y, parameters.crop.width, parameters.crop.height);
+                if (options.blit) {
+                    print('Blitting ' + options.blit.src + ' at ' + options.blit.x + ',' + options.blit.y);
+                    image.blit(options.blit.src, options.blit.x, options.blit.y);
+                }
+
+                if (options.composite) {
+                    print('Compositing ' + options.composite.src + ' at ' + options.composite.x + ',' + options.composite.y);
+                    image.composite(options.composite.src, options.composite.x, options.composite.y);
+                }
+
+                if (options.brightness !== undefined) {
+                    print('Adjusting brightness by ' + options.brightness);
+                    image.brightness(options.brightness);
+                }
+
+                if (options.contrast !== undefined) {
+                    print('Adjusting contrast by ' + options.contrast);
+                    image.contrast(options.contrast);
+                }
+
+                if (options.posterize !== undefined) {
+                    print('Posterizing image with ' + options.posterize + ' level');
+                    image.posterize(options.posterize);
+                }
+
+                if (options.mask) {
+                    print('Masking ' + options.mask.src + ' at ' + options.mask.x + ',' + options.mask.y);
+                    image.mask(options.mask.src, options.mask.x, options.mask.y);
+                }
+
+                if (options.dither565) {
+                    print('Dithering image, reducing colour space to 16 bits');
+                    image.dither565();
+                }
+
+                if (options.cover) {
+                    print('Covering image within ' + options.cover.width + 'x' + options.cover.height);
+                    image.cover(options.cover.width, options.cover.height);
+                }
+
+                if (options.contain) {
+                    print('Containing image within ' + options.contain.width + 'x' + options.contain.height);
+                    image.contain(options.contain.width, options.contain.height);
+                }
+
+                if (options.background) {
+                    print('Setting background colour to ' + options.background);
+                    image.background(options.background);
+                }
+
+                if (options.fade !== undefined) {
+                    print('Fading image by ' + options.fade);
+                    image.fade(options.fade);
+                }
+
+                if (options.opaque) {
+                    print('Setting the alpha channel on every pixel to fully opaque');
+                    image.opaque();
+                }
+
+                if (options.quality) {
+                    print('Setting quality level to ' + options.quality);
+                    image.quality(options.quality);
                 }
 
                 image.getBuffer(Jimp.MIME_PNG, function (error, buffer) {
-                    self.push(new gutil.File({ path: parameters.name, contents: buffer }));
+                    self.push(new gutil.File({ path: options.name, contents: buffer }));
                     return callback(error);
                 });
 
--- test/gulpfile.js
+++ test/gulpfile.js
@@ -4,14 +4,22 @@ var gulp = require('gulp'),
 gulp.task('default', function () {
     gulp.src('logo.png').pipe(jimp({
         name: 'logo-100.png',
-        resize: { width: 100, height: 100 },
         crop: { x: 100, y: 100, width: 200, height: 200 },
         invert: true,
+        flip: { horizontal: true, vertical: true },
+        gaussian: 2,
+        blur: 2,
         greyscale: true,
         sepia: true,
         opacity: 0.5,
+        resize: { width: 100, height: 100 },
         scale: 1.2,
-        blur: 2,
-        gaussian: 2
+        rotate: 90,
+        brightness: 0.5,
+        contrast: 0.3,
+        posterize: 2,
+        dither565: true,
+        background: '#ff0000',
+        logging: true
     })).pipe(gulp.dest('./images/'));
 });
